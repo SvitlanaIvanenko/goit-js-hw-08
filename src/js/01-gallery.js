7242593315c7b03646dab315056e3340ecb00f12
SvitlanaIvanenko <svitlana2404@gmail.com>
2022-10-05 22:03:31 +0300

    Two tasks are done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const imageTemplates = ({preview, original, description}) => 
+`<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+
+const render = () => {
+
+    const list = galleryItems.map(item => imageTemplates(item)).join('');
+    gallery.innerHTML = '';
+    gallery.insertAdjacentHTML('beforeend', list);
+};
+
+render ();
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt"});
