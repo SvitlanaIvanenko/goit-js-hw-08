fc3cc950ec1ef3eec126cecb4f43263f87b25743
SvitlanaIvanenko <svitlana2404@gmail.com>
2022-10-12 20:45:02 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+function onInput() {
+    const inputData = {
+      email: input.value,
+      message: textarea.value,
+    };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputData));
+  }
+  
+  function onSubmit (event) {
+    event.preventDefault();
+    if (input.value === '' || textarea.value === '') {
+      return window.alert('Please fill in all the fields');
+    }
+    console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    form.reset();
+  }
+
+function getSavedData() {
+    const storagedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  
+    if (storagedData) {
+      input.value = storagedData.email;
+      textarea.value = storagedData.message;
+    }
+  }
+  getSavedData();
